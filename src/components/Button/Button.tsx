5d10a40bbdd2c54ef2b02a4efe7495ab51790759
Luis Tupa <luis.tupa@orbis.com.pe>
2019-12-30 20:16:51 -0500

    feat(components): add Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ButtonStyled } from './styled';
+import { ButtonStyled, Loading } from './styled';
 
 interface Props {
-  children?: any
+  width?: number;
+  fullWidth?: boolean;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  size?: string;
+  variant?: string;
+  children?: any;
 }
 
-export const Button: React.FC<Props> = ({children}) => (
-  <ButtonStyled>{children}</ButtonStyled>
-);
+export const Button: React.FC<Props> = ({
+  width,
+  fullWidth,
+  isLoading,
+  isDisabled,
+  size,
+  variant, 
+  children 
+}) => {
+
+  if(isLoading) {
+    return(
+      <ButtonStyled
+        width={width}
+        fullWidth={fullWidth}
+        isLoading={isLoading}
+        size={size}
+        variant={variant}
+      >
+        <Loading variant={variant} />
+      </ButtonStyled>
+    );
+  }
+  
+  return(
+    <ButtonStyled
+      width={width}
+      fullWidth={fullWidth}
+      size={size}
+      variant={variant}
+      isDisabled={isDisabled}
+    >
+      {children}
+    </ButtonStyled>
+  );
+};
 
 Button.propTypes = {
+  width: PropTypes.number,
+  fullWidth: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  size: PropTypes.oneOf([
+    'small',
+    'normal',
+    'medium',
+    'large'
+  ]),
+  variant: PropTypes.oneOf([
+    'default',
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning'
+  ]),
   children: PropTypes.any
 }
+
+Button.defaultProps = {
+  size: 'normal',
+  variant: 'default'
+};
 
 Button.displayName = 'Button';
--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,20 +1,125 @@
-import styled from 'styled-components';
+import styled, { css, keyframes  } from 'styled-components';
+
+interface Props {
+  width?: number;
+  fullWidth?: boolean;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  size?: string;
+  variant?: string;
+}
+
+const COLORS = {
+  default: {
+    background: '#FFFFFF',
+    border: '#D8DDE1',
+    hover: '#D8DDE1',
+    text: '#5B656E'
+  },
+  primary: {
+    background: '#007BFF',
+    border: '#007BFF',
+    hover: '#0060C7',
+    text: '#FFFFFF',
+  },
+  secondary: {
+    background: '#5B656E',
+    border: '#5B656E',
+    hover: '#495158',
+    text: '#FFFFFF'
+  },
+  success: {
+    background: '#28A745',
+    border: '#28A745',
+    hover: '#1F7D35',
+    text: '#FFFFFF'
+  },
+  danger: {
+    background: '#DC3545',
+    border: '#DC3545',
+    hover: '#B92C39',
+    text: '#FFFFFF'
+  },
+  warning: {
+    background: '#F2A227',
+    border: '#F2A227',
+    hover: '#D08B21',
+    text: '#000000'
+  }
+};
+
+const SIZES = {
+  small: '0.75rem',
+  normal: '1rem',
+  medium: '1.25rem',
+  large: '1.5rem'
+}
+
+export const ButtonGroups = styled.div`
+  button {
+    margin: 5px;
+  }
+`;
 
 export const ButtonStyled = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   border: none;
-  background-color: #FFFFFF;
-  color: #5B656E;
-  padding: 5px 20px;
-  border: 1px solid #D8DDE1;
+  padding: 0 25px;
   border-radius: 4px;
   cursor: pointer;
   outline: none;
   font-weight: bold;
-  font-size: 16px;
+  min-height: 2.5em;
+  width: ${(props: Props) => props.fullWidth && '100%'};
+  width: ${(props: Props) => props.width && `${props.width}px`};
+  font-size: ${(props: Props) => props.size
+    && SIZES[props.size]};
 
-  &:hover {
-    background-color: #D8DDE1;
-  }
+  ${(props: Props) => props.variant && css`
+    background-color: ${COLORS[props.variant].background};
+    border: 1px solid ${COLORS[props.variant].border};
+    color: ${COLORS[props.variant].text};
+
+    &:hover {
+      background-color: ${COLORS[props.variant].hover};
+      transition: linear .4s;
+    }
+  `}
+
+  ${(props: Props) => props.isLoading && css`
+    pointer-events: none;
+    user-select: none;
+  `}
+
+  ${(props: Props) => props.isDisabled && css`
+    opacity: 0.5;
+    pointer-events: none;
+    user-select: none;
+  `}
 `;
 
+const animationLoading = keyframes`
+  to {transform: rotate(360deg);}
+`;
+
+export const Loading = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 2px solid black;
+  border-top: 2px solid white;
+  animation: ${animationLoading} .6s linear infinite;
+
+  ${(props: Props) => props.variant && css`
+    border: 2px solid ${COLORS[props.variant].text};
+    border-top: 2px solid ${COLORS[props.variant].background};
+  `}
+`;
+
+ButtonGroups.displayName = 'ButtonGroups';
 ButtonStyled.displayName = 'ButtonStyled';
+Loading.displayName = 'Loading';
